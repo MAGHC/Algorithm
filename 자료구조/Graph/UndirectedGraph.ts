b623279ead871478d1d2d922b063a3a49cdd731b
MAGHC <windowsapple95@gmail.com>
2024-04-02 21:08:12 +0900

    feat: removeEdge , removeVertex

--- 자료구조/Graph/UndirectedGraph.ts
+++ 자료구조/Graph/UndirectedGraph.ts
@@ -17,6 +17,20 @@ class UndirectedGraph {
     this.adjacencyList[vertex1].push(vertex2);
     this.adjacencyList[vertex2].push(vertex1);
   }
+
+  removeEdge(vertex1: string, vertex2: string) {
+    if (!this.adjacencyList[vertex1] || !this.adjacencyList[vertex2]) throw new Error('does not exisiting vertex');
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter((v) => v !== vertex2);
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter((v) => v !== vertex1);
+  }
+  removeVertex(vertex: string) {
+    if (!this.adjacencyList[vertex]) throw new Error('does not exisiting vertex');
+    while (this.adjacencyList[vertex].length) {
+      const poppedVertex = this.adjacencyList[vertex].pop() ?? '';
+      this.removeEdge(vertex, poppedVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
 }
 
 const newGraph = new UndirectedGraph();
